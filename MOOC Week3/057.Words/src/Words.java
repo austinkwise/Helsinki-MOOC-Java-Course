0b82921d34faa03c82ca6e92c4fde7bd2504ffc8
Austin Wise <austin.k.wise@gmail.com>
2018-12-31 11:42:07 -0500

    next batch of completed exercises

---
+++ MOOC Week3/057.Words/src/Words.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+      
+        while(true) {
+        	System.out.print("Type a word: ");
+            String text = reader.nextLine();
+	        if(text.isEmpty()) {
+	        	System.out.println("You typed the following words: ");
+	        	for (String word : words) {
+	        		System.out.println(word);
+	        		}
+	        	break;
+	        }
+	        else {
+	        	words.add(text);
+        }
+        }
+    }
+}
